b07b62fcbe90656450ac971a4524431576604cdb
aTractor7 <akovenkoura@gmail.com>
2025-05-23 00:26:38 +0300

    add chordPedia

--- app/src/main/java/com/example/guitarapp/data/remote/ChordApi.kt
+++ app/src/main/java/com/example/guitarapp/data/remote/ChordApi.kt
@@ -1,7 +1,6 @@
 package com.example.guitarapp.data.remote
 
 import com.example.guitarapp.data.model.Chord
-import com.example.guitarapp.data.model.SongShort
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
